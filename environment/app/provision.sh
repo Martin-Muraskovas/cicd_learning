e96b5d1999725b2f37b10d76d6fc317fffb188ee
Martin Muraskovas <MartinMurask123@googlemail.com>
2024-05-10 13:37:34 +0100

    test

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -15,7 +15,7 @@ sudo apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install nodejs -y
 
-
+# npm install
 sudo npm install
 # install pm2
 sudo npm install pm2 -g
