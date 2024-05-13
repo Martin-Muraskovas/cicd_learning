af432faa0fb357c8b45eb663cc911fd77202c85f
Martin Muraskovas <MartinMurask123@googlemail.com>
2024-05-13 10:35:05 +0100

    db prov changes

--- environment/db/provision.sh
+++ environment/db/provision.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# be careful of these keys, they will go out of date
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927
-echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-
-
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+sudo DEBIAN_FRONTEND=noninteractive apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg --batch -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+
+
 # sudo apt-get install mongodb-org=3.2.20 -y
-sudo apt-get install -y mongodb-org=3.2.20 mongodb-org-server=3.2.20 mongodb-org-shell=3.2.20 mongodb-org-mongos=3.2.20 mongodb-org-tools=3.2.20
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh=2.2.4 mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
 
 # remoe the default .conf and replace with our configuration
 sudo rm /etc/mongod.conf
